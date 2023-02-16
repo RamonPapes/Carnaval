237d68ddb0ec2c47b3e3d468d459a3cbb5c06c0a
RamonPapes <ramonpapes@gmail.com>
2023-02-16 18:00:21 -0300

    test: add console.log

--- router.js
+++ router.js
@@ -4,22 +4,22 @@ const routes = [
 ];
 
 function handleRoute() {
-  const path = window.location.pathname;
-  const route = routes.find((r) => r.path === path);
-
-  if (route) {
-    fetch(route.page)
-      .then((response) => response.text())
-      .then((html) => {
-        const container = document.getElementById('container');
-        container.innerHTML = html;
-      })
-      .catch((error) => {
-        console.error(`Failed to load page: ${route.page}. Error: ${error}`);
-      });
-  } else {
-    console.error(`No route found for path: ${path}`);
-  }
+    const path = window.location.pathname;
+    const route = routes.find((r) => r.path === path);
+    console.log(route);
+    if (route) {
+        fetch(route.page)
+        .then((response) => response.text())
+        .then((html) => {
+            const container = document.getElementById('container');
+            container.innerHTML = html;
+        })
+        .catch((error) => {
+            console.error(`Failed to load page: ${route.page}. Error: ${error}`);
+        });
+    } else {
+        console.error(`No route found for path: ${path}`);
+    }
 }
 
 window.addEventListener('popstate', handleRoute);
